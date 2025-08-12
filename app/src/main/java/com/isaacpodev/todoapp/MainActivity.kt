92540ac71ac94cc6f39ad7e2ced04f16d10ff591
IsaacPerugaOrtiga <isaacperuga03@gmail.com>
2025-08-12 10:21:56 +0200

    Ficheros adicionales

---
+++ app/src/main/java/com/isaacpodev/todoapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.isaacpodev.todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.isaacpodev.todoapp.addtasks.ui.TasksScreen
+import com.isaacpodev.todoapp.addtasks.ui.TasksViewModel
+import com.isaacpodev.todoapp.ui.theme.TodoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TodoAppTheme {
+                val tasksViewModel : TasksViewModel by viewModels()
+                TasksScreen(tasksViewModel)
+
+            }
+        }
+    }
+}
